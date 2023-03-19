8dae92fddeb2ed6c69769c5e39a9505f1dfb81cc
fadholi <fadholi@BA-00024815.local>
2023-03-19 08:42:02 +0700

    fix: fix logic

--- src/routes/home/index.ts
+++ src/routes/home/index.ts
@@ -9,7 +9,7 @@ const homePage = (app: App<StringIndexed>) => {
 
     app.event(HOME_OPENED, async ({ body, say }) => {
         const isTabMessage = body.event.tab === 'messages';
-        if (!isTabMessage) {
+        if (isTabMessage) {
             await say(APP_SETUP_MESSAGE);
         }
     });
