4641d4f6da1647f4a9292dfc14ba220f494061a2
fadholi <fadholi@BA-00024815.local>
2023-03-19 08:41:29 +0700

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ import homePage from './routes/home';
 
 dotenv.config();
 
-const slackApptoken = 'xapp-1-A04UZ4C8ERX-4969656527861-e3d25526a535f70085a5c050efebab95ec2fd04263beb25e79ab34dbd240aa79';
-const slackSigningSecret = '24fcb70f11ef1019843226c294c00abd';
-const slackAppBotToken = 'xoxb-2528249851142-4958070773287-huP7NLwVOXWjF5JQh5ZpG2Gh';
+const slackApptoken = process.env.SLACK_APP_TOKEN;
+const slackSigningSecret = process.env.SLACK_SIGNIN_SECRET;
+const slackAppBotToken = process.env.SLACK_APP_BOT_TOKEN;
 
 const app = new App({
   signingSecret: slackSigningSecret,
