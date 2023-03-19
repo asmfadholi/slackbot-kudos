07bff445e0d9c71f4bc431aadb5266e34652340d
fadholi <fadholi@BA-00024815.local>
2023-03-19 08:38:11 +0700

    chore: add gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { App } from '@slack/bolt';
-import homePage from './pages/home';
+import * as dotenv from 'dotenv';
+
+// routes
+import homePage from './routes/home';
+
+dotenv.config();
 
 const slackApptoken = 'xapp-1-A04UZ4C8ERX-4969656527861-e3d25526a535f70085a5c050efebab95ec2fd04263beb25e79ab34dbd240aa79';
 const slackSigningSecret = '24fcb70f11ef1019843226c294c00abd';
